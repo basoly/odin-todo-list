34ca33b5389f3b7f871ba43c638018ea95a4036b
basoly <lyanbasora@gmail.com>
2024-06-01 10:55:15 -0400

    Update addToList.js

--- src/addToList.js
+++ src/addToList.js
@@ -3,11 +3,16 @@
 const addToList = (newItem) => {
     // create html element
     var newElem = document.createElement('li');
-    newElem.classList.add('todo-item')
+    newElem.classList.add('todo-item');
+    newElem.setAttribute('id', newItem.id);
     //create button element
     var btn = document.createElement('button');
+    var label = document.createElement('div');
+    var dueDate = document.createElement('div');
+    var priority = document.createElement('div');
     btn.setAttribute('type', 'button');
     btn.innerHTML('▶')
+    
     
 
 
