832138157e34bf71806c27e2abf39153427dada8
basoly <lyanbasora@gmail.com>
2024-06-05 21:32:31 -0400

    Create renderList.js

---
+++ src/renderList.js
@@ -0,0 +1,25 @@
+const renderList = (list, array) => {
+  // to render lists to the DOM
+  
+  if (list == 'todoList') {
+    // delete current items in todo List container
+    
+    listContainer = document.querySelector()
+    listContainer.innerHTML = '';
+    // for each key, generate the DOM items
+    for (const item of array) {
+      
+    }
+    
+    // add to the respective container
+  } else if (list == 'projectList') {
+    // delete current items in projects container
+    // add to respective container
+    listContainer = document.querySelector();
+    listContainer.innerHTML = '';
+        // for each key, generate the DOM items
+    for (const item of array) {
+      
+    }
+  }
+}
