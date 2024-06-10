0941ab556f80e4bb7206c48b6bfa3cc1d15c24f2
LBD <lyanbasora@gmail.com>
2024-06-10 00:53:37 -0400

    Move empty files to new folder, will probably restart project

--- src/deleteItem.js
+++ src/deleteItem.js
@@ -3,11 +3,14 @@
 const deleteItem = (e) => {
     var id = e.target.parentNode.id;
     console.log(`This element's id is: ${id}`);
-    var removed = document.querySelector(`#${id}`);
-    console.log(removed);
-    var container = document.querySelector('.todo-container');
-    console.log(container)
-    container.removeChild(removed);
+    todoList.splice(todoList.indexOf(id), 1);
+    renderList('todoList', todoList);
+
+    // var removed = document.querySelector(`#${id}`);
+    // console.log(removed);
+    // var container = document.querySelector('.todo-container');
+    // console.log(container)
+    // container.removeChild(removed);
 }
 
 export default deleteItem;
--- src/renderList.js
+++ src/renderList.js
@@ -4,22 +4,82 @@ const renderList = (list, array) => {
   if (list == 'todoList') {
     // delete current items in todo List container
     
-    listContainer = document.querySelector()
+    var listContainer = document.querySelector('.todo-container')
     listContainer.innerHTML = '';
     // for each key, generate the DOM items
     for (const item of array) {
-      
+      var title = item.title;
+      var newElem = document.createElement('li');
+      newElem.classList.add('todo-item');
+      newElem.setAttribute('id', item.id);
+      // create button element
+      var btn = document.createElement('button');
+      var label = document.createElement('div');
+      var dueDate = document.createElement('div');
+      var priority = document.createElement('div');
+      var doneBtn = document.createElement('button');
+      var delBtn = document.createElement('button')
+      btn.setAttribute('type', 'button');
+      btn.innerHTML = '▶';
+      // label
+      label.classList.add('fields');
+      dueDate.classList.add('fields');
+      priority.classList.add('fields');
+      doneBtn.classList.add('done-btn');
+      doneBtn.setAttribute('type', 'button');
+      doneBtn.innerHTML = '✔';
+      delBtn.classList.add('delete-item');
+      delBtn.setAttribute('type', 'button');
+      delBtn.innerHTML = 'X';
+      // Transfering info from form input to DOM element
+      label.innerHTML = title;
+      dueDate.innerHTML = `<div>Due: ${item.due}</div>`;
+      priority.innerHTML = item.priority;
+
+      // add button event handlers
+      delBtn.addEventListener('click', deleteItem);
+      // add to item container
+      var elements = [btn, label, dueDate, priority, doneBtn, delBtn];
+
+      for (const elem of elements) {
+          newElem.appendChild(elem);
+      }
+  
+    
+      listContainer.appendChild(newElem);
+      console.log('To-do list updated');
     }
     
     // add to the respective container
   } else if (list == 'projectList') {
     // delete current items in projects container
     // add to respective container
-    listContainer = document.querySelector();
+    var listContainer = document.querySelector('.projects');
     listContainer.innerHTML = '';
         // for each key, generate the DOM items
     for (const item of array) {
-      
+      var title = item.title;
+      var newElem = document.createElement('li');
+      newElem.classList.add('proj-item');
+      newElem.setAttribute('id', item.id);
+      // add to DOM function
+      var titleContainer = document.createElement('div');
+      var delBtn = document.createElement('button');
+      delBtn.classList.add('delete-item');
+      delBtn.setAttribute('type', 'button');
+      delBtn.innerHTML = 'X';
+  
+      titleContainer = title;
+      delBtn.addEventListener('click', deleteItem);
+  
+      var elements = [titleContainer, delBtn]
+  
+      for (const elem of elements) {
+          newElem.appendChild(elem);
+      }
+  
+      listContainer.appendChild(newElem);
+      console.log('Project List updated')
     }
   }
 }
